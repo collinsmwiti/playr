1efb2e046e4e285440647a3f2bf340c68474e02f
Kenneth Gitere <gitere81@gmail.com>
2017-05-18 12:19:21 +0300

    change DBase for deploying

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.*;
 
 public class DB{
 
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/playr","Kenneth","Ee7BoiRVET");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-26-65.compute-1.amazonaws.com:5432/d1r2ogi6h8a552","osqtjkglokdijq","f06fbaf6a8aca459c278341a9e390453af699a1e625d70ab4d4b46dfc8e1851e");
 }
